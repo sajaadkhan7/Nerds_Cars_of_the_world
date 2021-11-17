75914c0255e5b0306f6b15c6ebe495616d6c42e8
YourKawarthaOldies <yourkawarhtaoldies2020@gmail.com>
2021-11-17 10:46:13 -0500

    About Page Update

--- Nerds_CodeFiles/aboutus.php
+++ Nerds_CodeFiles/aboutus.php
@@ -94,7 +94,7 @@
       <!-- Team item-->
       <div class="col-xl-3 col-sm-6 mb-5">
         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Jeny Patel</h5><span class="small text-uppercase text-muted">CEO</span>
+          <h5 class="mb-0">Jeni Patel</h5><span class="small text-uppercase text-muted">Senior Web Developer</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
